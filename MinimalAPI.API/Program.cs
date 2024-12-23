0bb08cac4a029c57b16028ddafb40ee7278b7a09
Soliv-code <solovyovilya@mail.ru>
2024-12-23 21:58:43 +0300

    Done

--- MinimalAPI.API/Program.cs
+++ MinimalAPI.API/Program.cs
@@ -9,6 +9,23 @@ builder.RegisterMediatR();
 
 var app = builder.Build();
 
+
+app.Use(async (ctx, next) =>
+{
+    try
+    {
+        await next();
+    }
+    catch (Exception) 
+    { 
+        ctx.Response.StatusCode = 500;
+        await ctx.Response.WriteAsync($"An error occurred, please contact the developers\nStatusCode: {ctx.Response.StatusCode}");
+    }
+});
+
+
+
+
 // Extensions/MinimalAPIExtensions.cs
 // EndpointDefinitions/EndpointDefinition.cs
 app.RegisterEndpointDefinitions();  
